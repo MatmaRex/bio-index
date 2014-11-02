7fb86d63b001f256d90aa2b60c23c1f392545055
Bartosz Dziewoński <matma.rex@gmail.com>
2014-11-02 17:01:50 +0100

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Update the on-wiki data from dump. Pass dump date as first parameter.
+
+rm plwiki-*-pages-meta-current.xml.bz2
+rm savepoint-*
+
+wget http://dumps.wikimedia.org/plwiki/$1/plwiki-$1-pages-meta-current.xml.bz2
+
+sed -i "s/dump_filename = 'plwiki-[0-9]*/dump_filename = 'plwiki-"$1"/" build-index.rb
+
+ruby build-index.rb
